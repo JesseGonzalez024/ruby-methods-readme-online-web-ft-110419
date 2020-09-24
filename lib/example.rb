f531851b44869dff294546ba352c7163a1f61198
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-09-24 19:33:34 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -2,16 +2,16 @@
 
 def greeting
   phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
+  # puts phrase
+  # puts phrase
+  # puts phrase
+  # puts phrase
+  # puts phrase
+  # puts phrase
+  # puts phrase
+  # puts phrase
+  # puts phrase
+  # puts phrase
 end
 
 greeting
