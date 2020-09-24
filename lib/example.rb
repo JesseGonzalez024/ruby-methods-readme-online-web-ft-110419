5a0e508beef601300ce26fd3237fb5108e87eebc
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-09-24 19:38:56 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -2,22 +2,14 @@
 
 def greeting
   phrase = "Hello World!"
-  # puts phrase
-  # puts phrase
-  # puts phrase
-  # puts phrase
-  # puts phrase
-  # puts phrase
-  # puts phrase
-  # puts phrase
-  # puts phrase
-  # puts phrase
   
   times = 0
+ 
   while times < 11 do
     puts phrase
     times += 1
   end
+
 end
 
 greeting
